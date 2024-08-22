404d440b2b916eb09fbe2c9de7734d7c031d0df6
Yusuf Rizal <yusufrizalh@gmail.com>
2024-08-22 19:53:33 +0700

    refactoring footer

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,48 +1,36 @@
-<footer class="text-center text-white mt-5" style="background-color: #cdcdcd;">
-    <!-- Grid container -->
+<!-- <footer class="text-center text-white mt-5" style="background-color: #cdcdcd;">
     <div class="container pt-4">
-        <!-- Section: Social media -->
         <section class="mb-4">
-            <!-- Facebook -->
             <a
                 data-mdb-ripple-init
                 class="btn btn-link btn-floating btn-lg text-body m-1"
                 href="#!"
                 role="button"
                 data-mdb-ripple-color="dark"><i class="fab fa-facebook-f"></i></a>
-
-            <!-- Twitter -->
             <a
                 data-mdb-ripple-init
                 class="btn btn-link btn-floating btn-lg text-body m-1"
                 href="#!"
                 role="button"
                 data-mdb-ripple-color="dark"><i class="fab fa-twitter"></i></a>
-
-            <!-- Google -->
             <a
                 data-mdb-ripple-init
                 class="btn btn-link btn-floating btn-lg text-body m-1"
                 href="#!"
                 role="button"
                 data-mdb-ripple-color="dark"><i class="fab fa-google"></i></a>
-
-            <!-- Instagram -->
             <a
                 data-mdb-ripple-init
                 class="btn btn-link btn-floating btn-lg text-body m-1"
                 href="#!"
                 role="button"
                 data-mdb-ripple-color="dark"><i class="fab fa-instagram"></i></a>
-
-            <!-- Linkedin -->
             <a
                 data-mdb-ripple-init
                 class="btn btn-link btn-floating btn-lg text-body m-1"
                 href="#!"
                 role="button"
                 data-mdb-ripple-color="dark"><i class="fab fa-linkedin"></i></a>
-            <!-- Github -->
             <a
                 data-mdb-ripple-init
                 class="btn btn-link btn-floating btn-lg text-body m-1"
@@ -50,16 +38,74 @@
                 role="button"
                 data-mdb-ripple-color="dark"><i class="fab fa-github"></i></a>
         </section>
-        <!-- Section: Social media -->
     </div>
-    <!-- Grid container -->
-
-    <!-- Copyright -->
     <div class="text-center p-3 bg-dark text-white" style="background-color: rgba(0, 0, 0, 0.05);">
         © 2024 Copyright:
         <a class="text-white text-decoration-none" href="https://inixindo.id">www.inixindo.id</a>
     </div>
-    <!-- Copyright -->
+</footer> -->
+
+<footer class="text-center text-white mt-5 pt-5" style="background-color: #393175;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 mb-5">
+                <h4 class="text-uppercase mb-4">Location</h4>
+                <p class="lead">
+                    Jl. Tenggilis Barat I/19 (D17) <br> Kota Surabaya
+                </p>
+            </div>
+            <div class="col-lg-4 mb-5">
+                <h4 class="text-uppercase mb-4">Social Media</h4>
+                <a
+                    data-mdb-ripple-init
+                    class="btn btn-link btn-floating btn-lg text-body m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"><i class="fab fa-facebook-f bg-white"></i></a>
+                <a
+                    data-mdb-ripple-init
+                    class="btn btn-link btn-floating btn-lg text-body m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"><i class="fab fa-twitter bg-white"></i></a>
+                <a
+                    data-mdb-ripple-init
+                    class="btn btn-link btn-floating btn-lg text-body m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"><i class="fab fa-google bg-white"></i></a>
+                <a
+                    data-mdb-ripple-init
+                    class="btn btn-link btn-floating btn-lg text-body m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"><i class="fab fa-instagram bg-white"></i></a>
+                <a
+                    data-mdb-ripple-init
+                    class="btn btn-link btn-floating btn-lg text-body m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"><i class="fab fa-linkedin bg-white"></i></a>
+                <a
+                    data-mdb-ripple-init
+                    class="btn btn-link btn-floating btn-lg text-body m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"><i class="fab fa-github bg-white"></i></a>
+            </div>
+            <div class="col-lg-4 mb-5">
+                <h4 class="text-uppercase mb-4">About Inixindo</h4>
+                <p class="lead">
+                    Lembaga Training dan Sertifikasi Terbaik
+                </p>
+            </div>
+        </div>
+
+    </div>
+    <div class="text-center p-3 fw-bold text-white" style="background-color: #eb4b88;">
+        © 2024 Copyright:
+        <a class="text-white text-decoration-none" href="https://inixindo.id">www.inixindo.id</a>
+    </div>
 </footer>
 
 <!-- Bootstrap JS -->
